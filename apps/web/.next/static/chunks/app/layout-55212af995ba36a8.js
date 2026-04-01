8463454486cd1de9f15b90d3a4ea3306ae69a18a
WebWorks Agent <accounts@bigleap.app>
2026-03-31 21:58:50 -0400

    Fix: add proper gitignore, remove build artifacts

--- apps/web/.next/static/chunks/app/layout-55212af995ba36a8.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{7821:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,8447,23))},8447:()=>{}},e=>{e.O(0,[280,256,937,358],()=>e(e.s=7821)),_N_E=e.O()}]);
